5c56b94b1ff5519452ee11ac8720e6f9987bcb4d
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2024-08-28 10:54:06 +0300

    add custom DoublyLinkedList in InMemoryHistoryManager

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,27 @@ public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = new Managers().getDefault();
         taskManager.createTask("t", "d");
+        taskManager.createTask("t", "d");
         taskManager.createEpic("t", "d");
-        taskManager.createSubtask(2, "t", "d");
+        taskManager.createSubtask(3, "t", "d");
+        taskManager.createSubtask(3, "t", "d");
+        taskManager.createSubtask(3, "t", "d");
+        taskManager.createEpic("t", "d");
         taskManager.getTaskById(1);
-        taskManager.getEpicById(2);
-        taskManager.getSubtaskById(3);
+        taskManager.getTaskById(2);
+        taskManager.getEpicById(3);
+        taskManager.getSubtaskById(4);
+        taskManager.getSubtaskById(5);
+        taskManager.getSubtaskById(6);
+        taskManager.getEpicById(7);
+        taskManager.deleteTask(1);
+        taskManager.deleteTask(2);
+        taskManager.deleteSubtask(4);
+        taskManager.deleteSubtask(5);
+        taskManager.deleteSubtask(6);
+        taskManager.deleteEpic(3);
+        taskManager.deleteEpic(7);
+
         for (Task task : taskManager.getHistory()) {
             System.out.println(task);
         }
--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -3,19 +3,37 @@ import org.junit.jupiter.api.Test;
 
 class InMemoryHistoryManagerTest {
     private final HistoryManager historyManager = new Managers().getDefaultHistory();
+    int taskId = 1;
+    int epicId = 2;
+    int subtaskId = 3;
 
     @Test
     void add() {
-        Task task = new Task(1, "t", "d");
+        Task task = new Task(taskId, "t", "d");
+        Epic epic = new Epic(epicId, "t", "d");
+        Subtask subtask = new Subtask(subtaskId, "t", "d");
         historyManager.add(task);
+        historyManager.add(epic);
+        historyManager.add(subtask);
         Assertions.assertTrue(historyManager.getHistory().contains(task));
+        Assertions.assertTrue(historyManager.getHistory().contains(epic));
+        Assertions.assertTrue(historyManager.getHistory().contains(subtask));
+    }
+
+    @Test
+    void remove() {
+        add();
+        historyManager.remove(taskId);
+        historyManager.remove(epicId);
+        historyManager.remove(subtaskId);
+        Assertions.assertTrue(historyManager.getHistory().isEmpty());
     }
 
     @Test
     void getHistory() {
-        int maxReturnedElements = 10;
+        int maxReturnedElements = 1000;
 
-        for (int i = 0; i < maxReturnedElements + 1; i++) {
+        for (int i = 0; i < maxReturnedElements; i++) {
             historyManager.add(new Task(i, "t", "d"));
         }
 
